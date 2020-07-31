384677775cf866fac0d19837d058d6358503b216
kompanycoder <mwegagideon@gmail.com>
2020-07-31 08:16:34 +0300

    Updates

---
+++ precache-manifest.38fea3c3b04a15debfe1aefb7ce97cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027cc8ead0057a7ede9e5164ad29f60a",
+    "url": "/user-manager-app/index.html"
+  },
+  {
+    "revision": "ae0eae9ae88fb5681c5f",
+    "url": "/user-manager-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6dd61e4cb56e763bd338",
+    "url": "/user-manager-app/static/css/main.7b699917.chunk.css"
+  },
+  {
+    "revision": "ae0eae9ae88fb5681c5f",
+    "url": "/user-manager-app/static/js/2.23516c22.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/user-manager-app/static/js/2.23516c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd61e4cb56e763bd338",
+    "url": "/user-manager-app/static/js/main.1234f265.chunk.js"
+  },
+  {
+    "revision": "9e476098705adee6f7e5",
+    "url": "/user-manager-app/static/js/runtime-main.16bfb8b5.js"
+  }
+]);
